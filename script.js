ec0f3ea6441d8af425be27e8fe2eb998418eb1ad
pjmantoss <joelptoss@gmail.com>
2020-04-17 16:19:23 +0100

    update script.js

--- script.js
+++ script.js
@@ -49,6 +49,17 @@ document.addEventListener('DOMContentLoaded', () => {
             if (leftEdge) direction = 1;
             else direction = -1;
         }
+        for (let i = 0; i <= alienInvaders.length - 1; i++){
+            squares[alienInvaders[i]].classList.remove('invader');
+        }
+        for (let i = 0; i <= alienInvaders.length - 1; i++){
+            alienInvaders[i] += direction;
+        }
+        for (let i = 0; i <= alienInvaders.length - 1; i++){
+            if (!alienInvadersTakenDown.includes(1)){
+                alienInvaders[i].classList.add('invader')
+            }
+        }
     }
 
 })
